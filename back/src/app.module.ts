4d474402a499212e9fe609344131784b484a81a1
JeronimoZapata <jerozapata13@gmail.com>
2025-10-09 15:03:06 -0300

    fix(src-api):delete unnecessary directory

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransporteModule } from './transporte/transporte.module';
 import { ShippingModule } from './shipping/shipping.module';
 
 @Module({
-  imports: [TransporteModule, ShippingModule],
+  imports: [ShippingModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/transporte/transporte.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransporteController } from './transporte.controller';
-
-@Module({
-  controllers: [TransporteController]
-})
-export class TransporteModule {}
