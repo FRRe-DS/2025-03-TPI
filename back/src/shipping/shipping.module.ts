fe83776f1b3c0b38c6bd32bc27eb1836e342996e
JeroZapata <jerozapata13@gmail.com>
2025-10-16 20:00:38 -0300

    add(api):v1.4, clearing unused code

--- back/src/shipping/shipping.module.ts
+++ back/src/shipping/shipping.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { ShippingController } from './shipping.controller';
 import { ShippingService } from './services/shipping.service';
-import { ShippingServicePagination } from './services/shipping-pagination.service';
-import { ShippingCancelService } from './services/shipping-cancel.service';
 
 @Module({
   controllers: [ShippingController],
   providers: [
     ShippingService,
-    ShippingServicePagination,
-    ShippingCancelService,
   ],
-  exports: [ShippingService, ShippingServicePagination, ShippingCancelService],
+  exports: [ShippingService],
 })
 export class ShippingModule {}
