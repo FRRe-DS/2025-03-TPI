dcf5a4cef95e5a4b7dafd386416c65b25e51e7bb
Alejo <alejoivanrojas@gmail.com>
2025-10-23 16:13:14 -0300

    refactor(shipping): fix crear envio page

--- front/src/types/logistica.ts
+++ front/src/types/logistica.ts
@@ -31,7 +31,7 @@ export interface ShippingCostResponse {
 }
 
 export interface ShippingCreationRequest {
-  user_id: number; 
+  user_id: number;
   delivery_address: Address;
   order_id: number;
   products: ProductItemInput[];
@@ -40,9 +40,9 @@ export interface ShippingCreationRequest {
 
 export interface ShippingCreationResponse {
   shipping_id: number;
-  status: string; 
-  transport_type: string; 
-  estimated_delivery_at: string; 
+  status: string;
+  transport_type: string;
+  estimated_delivery_at: string;
 }
 
 export interface TransportMethod {
@@ -71,17 +71,8 @@ export interface ShipmentProduct {
 }
 
 export interface ShippingResponse {
-  id: number;
-  user: {
-    id: number;
-  };
-  originAddress: AddressWithId;
-  destinationAddress: AddressWithId;
-  date: string;
+  shipping_id: number;
   status: string;
-  transportMethod: TransportMethod;
-  totalCost: string;
-  createdAt: string;
-  updatedAt: string;
-  shipmentProducts: ShipmentProduct[];
+  transport_type: string;
+  estimated_delivery_at: string;
 }
